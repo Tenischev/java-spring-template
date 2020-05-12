595bdb0b1b8d2859f4d5322ad20000c633eea583
Semen Tenishchev <tenischev.semen@gmail.com>
2020-05-12 22:00:53 +0300

    feat: unify model generators between java templates

--- filters/all.js
+++ filters/all.js
@@ -1,5 +1,178 @@
 const filter = module.exports;
 const _ = require('lodash');
+const ScsLib = require('../lib/scsLib.js');
+const scsLib = new ScsLib();
+
+function identifierName(str) {
+  return scsLib.getIdentifierName(str);
+}
+filter.identifierName = identifierName;
+
+function indent(numTabs) {
+  return "\t".repeat(numTabs);
+}
+
+function indent1(numTabs) {
+  return indent(numTabs);
+}
+filter.indent1 = indent1;
+
+function indent2(numTabs) {
+  return indent(numTabs + 1);
+}
+filter.indent2 = indent2;
+
+function indent3(numTabs) {
+  return indent(numTabs + 2);
+}
+filter.indent3 = indent3;
+
+// This returns the list of methods belonging to an object, just to help debugging.
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
+
+function schemaExtraIncludes([schemaName, schema]) {
+  console.log("checkPropertyNames " + schemaName + "  " + schema.type());
+  let ret = {};
+  if(checkPropertyNames(schemaName, schema)) {
+    ret.needJsonPropertyInclude = true;
+  }
+  console.log("checkPropertyNames:");
+  console.log(ret);
+  return ret;
+}
+filter.schemaExtraIncludes = schemaExtraIncludes;
+
+// Returns true if any property names will be different between json and java.
+function checkPropertyNames(name, schema) {
+  let ret = false;
+
+  console.log(JSON.stringify(schema));
+  console.log('checkPropertyNames: checking schema ' + name + getMethods(schema));
+
+  var properties = schema.properties();
+
+
+  if (schema.type() === 'array') {
+    properties = schema.items().properties();
+  }
+
+  console.log("schema type: " + schema.type());
+
+  for (let propName in properties) {
+    let javaName = _.camelCase(propName);
+    let prop = properties[propName];
+    console.log('checking ' + propName + ' ' + prop.type());
+
+    if (javaName !== propName) {
+      console.log("Java name " + javaName + " is different from " + propName);
+      return true;
+    }
+    if (prop.type() === 'object') {
+      console.log("Recursing into object");
+      let check = checkPropertyNames(propName, prop);
+      if (check) {
+        return true;
+      }
+    } else if (prop.type() === 'array') {
+      console.log('checkPropertyNames: ' + JSON.stringify(prop));
+      if (!prop.items) {
+        throw new Error("Array named " + propName + " must have an 'items' property to indicate what type the array elements are.");
+      }
+      let itemsType = prop.items.type;
+      console.log('checkPropertyNames: ' + JSON.stringify(prop.items));
+      console.log('array of : ' + itemsType);
+      if (itemsType === 'object') {
+        console.log("Recursing into array");
+        let check = checkPropertyNames(propName, prop.items);
+        if (check) {
+          return true;
+        }
+      }
+    }
+  }
+  return ret;
+}
+
+// This maps json schema types to Java types.
+const typeMap = new Map();
+typeMap.set('boolean', 'Boolean');
+typeMap.set('integer', 'Integer');
+typeMap.set('null', 'String');
+typeMap.set('number', 'Double');
+typeMap.set('string', 'String');
+
+// This returns the proper Java type for a schema property.
+function fixType([name, javaName, property]) {
+
+  //console.log('fixType: ' + name + " " + dump(property));
+
+  let isArrayOfObjects = false;
+
+  // For message headers, type is a property.
+  // For schema properties, type is a function.
+  let type = property.type;
+
+  //console.log("fixType: " + property);
+
+  if (typeof type == "function") {
+    type = property.type();
+  }
+
+  // If a schema has a property that is a ref to another schema,
+  // the type is undefined, and the title gives the title of the referenced schema.
+  let ret;
+  if (type === undefined) {
+    if (property.enum()) {
+      ret = _.upperFirst(javaName);
+    } else {
+      // check to see if it's a ref to another schema.
+      ret = property.ext('x-parser-schema-id');
+
+      if (!ret) {
+        throw new Error("Can't determine the type of property " + name);
+      }
+    }
+  } else if (type === 'array') {
+    if (!property.items()) {
+      throw new Error("Array named " + name + " must have an 'items' property to indicate what type the array elements are.");
+    }
+    let itemsType = property.items().type();
+
+    if (itemsType) {
+
+      if (itemsType === 'object') {
+        isArrayOfObjects = true;
+        itemsType = _.upperFirst(javaName);
+      } else {
+        itemsType = typeMap.get(itemsType);
+      }
+    }
+    if (!itemsType) {
+      itemsType = property.items().ext('x-parser-schema-id');
+
+      if (!itemsType) {
+        throw new Error("Array named " + name + ": can't determine the type of the items.");
+      }
+    }
+    ret = _.upperFirst(itemsType) + "[]";
+  } else if (type === 'object') {
+    ret = _.upperFirst(javaName);
+  } else {
+    ret = typeMap.get(type);
+    if (!ret) {
+      ret = type;
+    }
+  }
+  return [ret, isArrayOfObjects];
+}
+filter.fixType = fixType;
 
 function toJavaType(str){
   switch(str) {
--- template/src/main/java/com/asyncapi/model/$$schema$$.java
+++ template/src/main/java/com/asyncapi/model/$$schema$$.java
@@ -1,68 +1,9 @@
 package com.asyncapi.model;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
+{% set extraIncludes = [schemaName, schema] | schemaExtraIncludes %}
+import com.fasterxml.jackson.annotation.JsonInclude;
+{% if extraIncludes.needJsonPropertyInclude -%}
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-{% if schema.description() or schema.examples() %}/**{% for line in schema.description() | splitByLines %}
- * {{ line | safe}}{% endfor %}{% if schema.examples() %}
- * Examples: {{schema.examples() | examplesToString | safe}}{% endif %}
- */{% endif %}
-public class {{schemaName | camelCase | upperFirst}} {
-    {% for propName, prop in schema.properties() %}
-        {%- if prop.type() === 'object' %}
-    private @Valid {{prop.uid() | camelCase | upperFirst}} {{propName | camelCase}};
-        {%- elif prop.type() === 'array' %}
-            {%- if prop.items().format() %}
-    private @Valid {{prop.items().format() | toJavaType}}[] {{propName | camelCase}}Array;
-            {%- else %}
-    private @Valid {{prop.items().type() | toJavaType}}[] {{propName | camelCase}}Array;
-            {%- endif %}
-        {%- else %}
-            {%- if prop.format() %}
-    private @Valid {{prop.format() | toJavaType}} {{propName | camelCase}};
-            {%- else %}
-    private @Valid {{prop.type() | toJavaType}} {{propName | camelCase}};
-            {%- endif %}
-        {%- endif %}
-    {% endfor %}
-
-    {% for propName, prop in schema.properties() %}
-        {%- set varName = propName | camelCase %}
-        {%- set className = propName | camelCase | upperFirst %}
-        {%- if prop.type() === 'object' %}
-            {%- set propType = prop.uid() | camelCase | upperFirst %}
-        {%- elif prop.type() === 'array' %}
-            {%- set varName = propName | camelCase + 'Array' %}
-            {%- if prop.items().format() %}
-                {%- set propType = prop.items().format() | toJavaType + '[]' %}
-            {%- else %}
-                {%- set propType = prop.items().type() | toJavaType + '[]' %}
-            {%- endif %}
-        {%- else %}
-            {%- if prop.format() %}
-                {%- set propType = prop.format() | toJavaType %}
-            {%- else %}
-                {%- set propType = prop.type() | toJavaType %}
-            {%- endif %}
-        {%- endif %}
-
-    {% if prop.description() or prop.examples()%}/**{% for line in prop.description() | splitByLines %}
-     * {{ line | safe}}{% endfor %}{% if prop.examples() %}
-     * Examples: {{prop.examples() | examplesToString | safe}}{% endif %}
-     */{% endif %}
-    @JsonProperty("{{propName}}")
-    {%- if propName | isRequired(schema.required()) %}@NotNull{% endif %}
-    {%- if prop.minLength() or prop.maxLength() or prop.maxItems() or prop.minItems() %}@Size({% if prop.minLength() or prop.minItems() %}min = {{prop.minLength()}}{{prop.minItems()}}{% endif %}{% if prop.maxLength() or prop.maxItems() %}{% if prop.minLength() or prop.minItems() %},{% endif %}max = {{prop.maxLength()}}{{prop.maxItems()}}{% endif %}){% endif %}
-    {%- if prop.pattern() %}@Pattern(regexp="{{prop.pattern()}}"){% endif %}
-    {%- if prop.minimum() %}@Min({{prop.minimum()}}){% endif %}{% if prop.exclusiveMinimum() %}@Min({{prop.exclusiveMinimum() + 1}}){% endif %}
-    {%- if prop.maximum() %}@Max({{prop.maximum()}}){% endif %}{% if prop.exclusiveMaximum() %}@Max({{prop.exclusiveMaximum() + 1}}){% endif %}
-    public {{propType}} get{{className}}() {
-        return {{varName}};
-    }
-
-    public void set{{className}}({{propType}} {{varName}}) {
-        this.{{varName}} = {{varName}};
-    }
-    {% endfor %}
-}
+{% endif %}
+{% from "partials/JavaClass.java" import javaClass -%}
+{{ javaClass(schemaName, schema, schema.properties(), schema.required(), 0, false ) }}
